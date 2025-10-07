a1f2d45f64cb5779d905ffe0d214a88c902e3526
Filip <fizajac@student.agh.edu.pl>
2025-10-07 17:29:17 +0200

    nowy plik

---
+++ Lab1/program.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Program{
+	public static void main(String[] args){
+		/*Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Podaj wysokosc choinki");
+		
+		int wysokosc = scanner.nextInt();*/
+		System.out.println("*");
+		int h1 = Integer.parseInt(args[0]);
+		
+		for (int i=1; i<h1; i++)
+		{
+			int pomocnicza = 0;
+			
+			while (pomocnicza != i+1)
+			{
+				System.out.print("*");
+				pomocnicza++;
+			}
+			System.out.println("");
+
+		}
+	
+	
+	
+	}
+	
+	
+
+
+}

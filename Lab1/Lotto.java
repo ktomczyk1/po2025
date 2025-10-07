73106409ce5b8a004ee76b6b2a0d232847f6b76f
Filip <fizajac@student.agh.edu.pl>
2025-10-07 17:53:16 +0200

    zmiany

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,20 +1,25 @@
 import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Lotto {
     public static void main(String[] args) {
         Random rand = new Random();
-        ArrayList<Integer> liczby = new ArrayList<Integer>();
+        //ArrayList<Integer> liczby = new ArrayList<Integer>();
+        HashSet<Integer> liczby_bez_powtorek = new HashSet<Integer>();
 
         System.out.println("Z maszyny losującej wyciagamy nastepujace liczby:");
+        while (liczby_bez_powtorek.size() < 5) {
+            for (int i = 0; i<6; i++)
+            {
+                int n = rand.nextInt(50);
+                liczby_bez_powtorek.add(n);
+            }
 
-        for (int i = 0; i<6; i++)
-        {
-            int n = rand.nextInt(50);
-            liczby.add(n);
         }
-        System.out.println(liczby);
+        System.out.println(liczby_bez_powtorek);
+
     }
 }
 

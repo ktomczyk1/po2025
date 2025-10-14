c996838a14673d1a6ff1972618e4642c50eca6e4
Filip <fizajac@student.agh.edu.pl>
2025-10-14 16:59:49 +0200

    ssssssssssssssssssssssssssssss

--- Lab1/src/Lotto.java
+++ Lab1/src/Lotto.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.Random;
-import java.util.ArrayList;
 import java.util.HashSet;
 
 public class lotto {
--- Lab2/src/Lotto.java
+++ Lab2/src/Lotto.java
@@ -1,3 +1,5 @@
 public class Lotto {
+    public static void main(String args[]) {
 
+    }
 }

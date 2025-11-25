bc5499de12c3e18992dbb1d8667938e3d2c9408b
Filip <fizajac@student.agh.edu.pl>
2025-11-25 18:53:34 +0100

    wwww

---
+++ symulator_gui/src/main/java/org/example/symulator_gui/MainFXML.java
@@ -0,0 +1,22 @@
+package org.example.symulator_gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFXML extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFXML.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 650);
+        stage.setTitle("Symulator (FXML)");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
